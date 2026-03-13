b9fbc0cfcd72e6eba727aa39883ccca54bae3ae7
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-03-13 03:21:52 +0100

    Update login.js

--- api/whop/login.js
+++ api/whop/login.js
@@ -1,11 +1,9 @@
 export default async function handler(req, res) {
-
   const clientId = process.env.WHOP_CLIENT_ID;
   const redirectUri = process.env.WHOP_REDIRECT_URI;
 
   const authUrl =
-    `https://api.whop.com/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code`;
+    `https://api.whop.com/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=openid%20profile%20email`;
 
   res.redirect(authUrl);
-
 }
