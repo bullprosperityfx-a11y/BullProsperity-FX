17d57b5a1d9462a00db4116744073487d65719dd
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-03-16 13:07:05 +0100

    Delete api/whop/login.js

--- api/whop/login.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const startButton = document.getElementById("startButton");
-
-  if (startButton) {
-    startButton.href = "https://google.com";
-    startButton.textContent = "Jetzt starten";
-  }
-});
