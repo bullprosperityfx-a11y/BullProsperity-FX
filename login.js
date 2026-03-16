b01b043a1d8b1e37b9dc6a0286adfcca2969b156
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-03-16 13:06:30 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const startButton = document.getElementById("startButton");
 
+  if (startButton) {
+    startButton.href = "https://google.com";
+    startButton.textContent = "Jetzt starten";
+  }
+});
