1a95f3534320f8d9e6d47ffc73c4718de0230584
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-04-14 21:06:30 +0200

    Update login.js

--- api/whop/login.js
+++ api/whop/login.js
@@ -1,7 +1,29 @@
 export default function handler(req, res) {
-  const url = "https://api.whop.com/oauth/authorize?response_type=code"
-    + "&client_id=" + process.env.WHOP_CLIENT_ID
-    + "&redirect_uri=" + process.env.WHOP_REDIRECT_URI;
+  try {
+    const clientId = process.env.WHOP_CLIENT_ID;
+    const redirectUri = process.env.WHOP_REDIRECT_URI;
 
-  res.redirect(url);
+    // DEBUG OUTPUT
+    if (!clientId) {
+      return res.status(500).send("CLIENT_ID FEHLT");
+    }
+
+    if (!redirectUri) {
+      return res.status(500).send("REDIRECT_URI FEHLT");
+    }
+
+    const url =
+      "https://api.whop.com/oauth/authorize?" +
+      new URLSearchParams({
+        response_type: "code",
+        client_id: clientId,
+        redirect_uri: redirectUri,
+        scope: "openid email"
+      }).toString();
+
+    return res.redirect(url);
+
+  } catch (err) {
+    return res.status(500).send("LOGIN CRASH: " + err.message);
+  }
 }
