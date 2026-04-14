25263af22b524b568ba46fe84724a9b6b9490dd4
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-04-14 19:30:53 +0200

    Update login.js

--- api/whop/login.js
+++ api/whop/login.js
@@ -1,17 +1,3 @@
 export default function handler(req, res) {
-  const clientId = process.env.WHOP_CLIENT_ID;
-
-  const redirectUri =
-    "https://bull-prosperity-fx.vercel.app/api/whop/callback";
-
-  const url =
-    "https://api.whop.com/oauth/authorize?" +
-    new URLSearchParams({
-      response_type: "code",
-      client_id: clientId,
-      redirect_uri: redirectUri,
-      scope: "openid email"
-    }).toString();
-
-  res.redirect(url);
+  res.status(200).send("LOGIN WORKS");
 }
