6a94ecc6f19490df82d03e4fae9fa9c0da69bf03
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-04-14 21:10:20 +0200

    Update login.js

--- api/whop/login.js
+++ api/whop/login.js
@@ -1,3 +1,21 @@
 export default function handler(req, res) {
-  return res.send("LOGIN FUNCTION WORKS");
+  try {
+    const clientId = process.env.WHOP_CLIENT_ID;
+    const redirectUri = process.env.WHOP_REDIRECT_URI;
+
+    if (!clientId || !redirectUri) {
+      return res.status(500).send("ENV FEHLT");
+    }
+
+    const url =
+      "https://api.whop.com/oauth/authorize" +
+      "?response_type=code" +
+      "&client_id=" + encodeURIComponent(clientId) +
+      "&redirect_uri=" + encodeURIComponent(redirectUri);
+
+    return res.redirect(url);
+
+  } catch (err) {
+    return res.status(500).send("ERROR: " + err.message);
+  }
 }
