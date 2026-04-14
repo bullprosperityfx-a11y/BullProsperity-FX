e2f2ca2d1d482cae994952005399393da4147733
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-04-14 18:35:35 +0200

    Update login.js

--- api/whop/login.js
+++ api/whop/login.js
@@ -1,21 +1,15 @@
-export default async function handler(req, res) {
-  try {
-    const clientId = process.env.WHOP_CLIENT_ID;
-    const redirectUri = process.env.WHOP_REDIRECT_URI;
+export default function handler(req, res) {
+  const clientId = process.env.WHOP_CLIENT_ID;
+  const redirectUri = process.env.WHOP_REDIRECT_URI;
 
-    const url =
-      "https://api.whop.com/oauth/authorize?" +
-      new URLSearchParams({
-        response_type: "code",
-        client_id: clientId,
-        redirect_uri: redirectUri,
-        scope: "openid email"
-      }).toString();
+  const url =
+    "https://api.whop.com/oauth/authorize?" +
+    new URLSearchParams({
+      response_type: "code",
+      client_id: clientId,
+      redirect_uri: redirectUri,
+      scope: "openid email"
+    }).toString();
 
-    return res.redirect(url);
-
-  } catch (err) {
-    console.error("LOGIN ERROR:", err);
-    return res.status(500).send("Login failed");
-  }
+  res.redirect(url);
 }
