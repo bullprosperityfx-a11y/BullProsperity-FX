1eba01f1f1ae0842e55e3179a0765788223f756c
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-05-07 12:36:03 +0200

    Create supabase.js

---
+++ admin/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "sb_publishable_5vZ4w9Y40QDAthZIbrWJqg_9s7Fv5Vb";
+const SUPABASE_ANON_KEY = "https://bygrocbckwjcatrgdook.supabase.co/rest/v1/";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
