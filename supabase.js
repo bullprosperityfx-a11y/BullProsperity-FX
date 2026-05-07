1accfe321d329f6f10038c43908d27ed3396c8aa
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-05-07 12:55:33 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://bygrocbckwjcatrgdook.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://bygrocbckwjcatrgdook.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_5vZ4w9Y40QDAthZIbrWJqg_9s7Fv5Vb";
 
 const supabaseClient = supabase.createClient(
