485a9142f1f395d23607eae861f7488c40d20a9d
bullprosperityfx-a11y <bullprosperityfx@gmail.com>
2026-05-11 15:02:25 +0200

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,9 +1,11 @@
 export default async function handler(req, res) {
   res.setHeader("Set-Cookie", [
-    "bp_role=; Path=/; HttpOnly; Max-Age=0; SameSite=Lax",
-    "bp_email=; Path=/; HttpOnly; Max-Age=0; SameSite=Lax",
-    "whop_access_token=; Path=/; HttpOnly; Max-Age=0; SameSite=Lax"
+    "bp_role=; Path=/; Max-Age=0; SameSite=Lax; Secure",
+    "bp_email=; Path=/; Max-Age=0; SameSite=Lax; Secure",
+    "whop_access_token=; Path=/; Max-Age=0; SameSite=Lax; Secure",
+    "whop_state=; Path=/; Max-Age=0; SameSite=Lax; Secure",
+    "whop_verifier=; Path=/; Max-Age=0; SameSite=Lax; Secure"
   ]);
 
-  return res.redirect("/");
+  return res.status(200).json({ ok: true });
 }
